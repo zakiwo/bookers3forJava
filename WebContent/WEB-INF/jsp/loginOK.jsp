242fed65b440a6351373e9af30a62ebc4bd2257a
zakiwo <hiroki.ozaki39@gmail.com>
2021-12-03 17:33:03 +0900

    chante logoinOK.jsp

--- WebContent/WEB-INF/jsp/loginOK.jsp
+++ WebContent/WEB-INF/jsp/loginOK.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +8,7 @@
 <title>Bookers3</title>
 </head>
 <body>
-<p>ようこそ{ユーザーID}さん</p>
+<p>ようこそ<c:out value="${ userId }" />さん</p>
 <a href="/bookers3/WelcomeServlet">トップへ</a>
 </body>
 </html>
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Login;
+import model.LoginLogic;
 
 /**
  * Servlet implementation class LoginServlet
@@ -27,6 +30,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//ログイン画面にフォワード
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -35,8 +39,29 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginOK.jsp");
-		dispatcher.forward(request, response);
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String userId = request.getParameter("userId");
+		String pass = request.getParameter("pass");
+		
+		//ログイン処理の実行
+		Login login = new Login(userId, pass);
+		LoginLogic bo = new LoginLogic();
+		//ログインできるかをresultに格納
+		boolean result = bo.execute(login);
+		
+		if(result) {
+			//ログインできれば、セッションスコープにユーザーIDを保存
+			HttpSession session = request.getSession();
+			session.setAttribute("userId", userId);
+			
+			//ログイン完了画面にフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginOK.jsp");
+			dispatcher.forward(request, response);
+		}else {
+			//ログインに失敗したらログイン画面にリダイレクト
+			response.sendRedirect("/bookers3/LoginServlet");
+		}
 	}
 
 }
